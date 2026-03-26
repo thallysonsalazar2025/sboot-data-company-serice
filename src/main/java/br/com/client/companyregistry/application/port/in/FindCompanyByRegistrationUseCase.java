e143be7dcf1b693a7ce5faa04ac7bfa10e7e0aac
Thallyson pontes <242952126+thallysonsalazar2025@users.noreply.github.com>
2026-03-26 14:48:16 -0300

    Inclui melhorias no fluxo aponta a consulta para o banco em memoria

--- src/main/java/br/com/client/companyregistry/application/port/in/FindCompanyByRegistrationUseCase.java
+++
@@ -1,9 +0,0 @@
-package br.com.client.companyregistry.application.port.in;
-
-import br.com.client.companyregistry.domain.model.CompanyRegistryEntry;
-
-import java.util.Optional;
-
-public interface FindCompanyByRegistrationUseCase {
-    Optional<CompanyRegistryEntry> find(String registrationNumber, String countryCode);
-}
